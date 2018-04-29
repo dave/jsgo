6ecd3f2254d92bfb409937b7a810ce73082a1b65
Dave Brophy <dave@brophy.uk>
2018-04-29 11:21:15 +0200

    Retry with clone if fetch fails

--- services/gitfetcher/fetcher.go
+++ services/gitfetcher/fetcher.go
@@ -31,18 +31,10 @@ type Fetcher struct {
 
 func (f *Fetcher) Fetch(ctx context.Context, url string) (billy.Filesystem, error) {
 
-	persisted := memfs.New()
-
-	sfs, err := sivafs.NewFilesystem(persisted, FNAME, memfs.New())
+	persisted, sfs, store, worktree, err := initFilesystems()
 	if err != nil {
 		return nil, err
 	}
-
-	store, err := filesystem.NewStorage(sfs)
-	if err != nil {
-		return nil, err
-	}
-	worktree := memfs.New()
 
 	exists, err := load(ctx, f.cache, url, persisted)
 	if err != nil {
@@ -59,78 +51,20 @@ func (f *Fetcher) Fetch(ctx context.Context, url string) (billy.Filesystem, erro
 	var changed bool
 
 	if exists {
-		// Opening git repo
-		repo, err := git.Open(store, worktree)
-		if err != nil {
-			return nil, err
-		}
-
-		// Get the origin remote (all repos have origin?)
-		remote, err := repo.Remote("origin")
-		if err != nil {
-			return nil, err
-		}
-
-		// Get a list of references from the remote
-		refs, err := remote.List(&git.ListOptions{})
-		if err != nil {
-			return nil, err
-		}
-
-		// Find the HEAD reference. If we can't find it, return an error.
-		rs := memory.ReferenceStorage{}
-		for _, ref := range refs {
-			rs[ref.Name()] = ref
-		}
-		originHead, err := storer.ResolveReference(rs, plumbing.HEAD)
-		if err != nil {
-			return nil, err
-		}
-		if originHead == nil {
-			return nil, errors.New("HEAD not found")
-		}
-
-		// We only need to do a full Fetch if the head has changed. Compare with repo.Head().
-		repoHead, err := repo.Head()
-		if err != nil {
-			return nil, err
-		}
-		if originHead.Hash() != repoHead.Hash() {
-
-			// repo has changed - this will mean it's saved after the operation
-			changed = true
-
-			// Do a full Fetch. Can this be made faster with options?
-			if err := repo.Fetch(&git.FetchOptions{
-				Force: true,
-			}); err != nil && err != git.NoErrAlreadyUpToDate {
+		if changed, err = doFetch(store, worktree); err != nil {
+			// If error while fetching, try a full clone before exiting. Make sure we re-initialise
+			// the filesystems.
+			persisted, sfs, store, worktree, err = initFilesystems()
+			if err != nil {
+				return nil, err
+			}
+			if changed, err = doClone(url, store, worktree); err != nil {
 				return nil, err
 			}
 		}
 
-		// Get the worktree, and do a hard reset to the HEAD from origin.
-		w, err := repo.Worktree()
-		if err != nil {
-			return nil, err
-		}
-		if err := w.Reset(&git.ResetOptions{
-			Commit: originHead.Hash(),
-			Mode:   git.HardReset,
-		}); err != nil {
-			return nil, err
-		}
-
 	} else {
-
-		// repo has changed - this will mean it's saved after the operation
-		changed = true
-
-		// Clone the repo
-		if _, err := git.Clone(store, worktree, &git.CloneOptions{
-			URL:          url,
-			Tags:         git.NoTags,
-			SingleBranch: true,
-		}); err != nil {
+		if changed, err = doClone(url, store, worktree); err != nil {
 			return nil, err
 		}
 	}
@@ -146,6 +80,102 @@ func (f *Fetcher) Fetch(ctx context.Context, url string) (billy.Filesystem, erro
 	go save(gitctx, f.cache, url, persisted)
 
 	return worktree, nil
+}
+
+func initFilesystems() (persisted billy.Filesystem, sfs sivafs.SivaFS, store *filesystem.Storage, worktree billy.Filesystem, err error) {
+
+	persisted = memfs.New()
+
+	sfs, err = sivafs.NewFilesystem(persisted, FNAME, memfs.New())
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	store, err = filesystem.NewStorage(sfs)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	worktree = memfs.New()
+
+	return persisted, sfs, store, worktree, nil
+}
+
+func doFetch(store *filesystem.Storage, worktree billy.Filesystem) (changed bool, err error) {
+	// Opening git repo
+	repo, err := git.Open(store, worktree)
+	if err != nil {
+		return false, err
+	}
+
+	// Get the origin remote (all repos have origin?)
+	remote, err := repo.Remote("origin")
+	if err != nil {
+		return false, err
+	}
+
+	// Get a list of references from the remote
+	refs, err := remote.List(&git.ListOptions{})
+	if err != nil {
+		return false, err
+	}
+
+	// Find the HEAD reference. If we can't find it, return an error.
+	rs := memory.ReferenceStorage{}
+	for _, ref := range refs {
+		rs[ref.Name()] = ref
+	}
+	originHead, err := storer.ResolveReference(rs, plumbing.HEAD)
+	if err != nil {
+		return false, err
+	}
+	if originHead == nil {
+		return false, errors.New("HEAD not found")
+	}
+
+	// We only need to do a full Fetch if the head has changed. Compare with repo.Head().
+	repoHead, err := repo.Head()
+	if err != nil {
+		return false, err
+	}
+	if originHead.Hash() != repoHead.Hash() {
+
+		// repo has changed - this will mean it's saved after the operation
+		changed = true
+
+		// Do a full Fetch. Can this be made faster with options?
+		if err := repo.Fetch(&git.FetchOptions{
+			Force: true,
+		}); err != nil && err != git.NoErrAlreadyUpToDate {
+			return false, err
+		}
+	}
+
+	// Get the worktree, and do a hard reset to the HEAD from origin.
+	w, err := repo.Worktree()
+	if err != nil {
+		return false, err
+	}
+	if err := w.Reset(&git.ResetOptions{
+		Commit: originHead.Hash(),
+		Mode:   git.HardReset,
+	}); err != nil {
+		return false, err
+	}
+
+	return changed, nil
+}
+
+func doClone(url string, store *filesystem.Storage, worktree billy.Filesystem) (changed bool, err error) {
+	// Clone the repo
+	if _, err := git.Clone(store, worktree, &git.CloneOptions{
+		URL:          url,
+		Tags:         git.NoTags,
+		SingleBranch: true,
+	}); err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
 func save(ctx context.Context, fileserver services.Fileserver, url string, fs billy.Filesystem) error {
