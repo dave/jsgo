7d63299b6931705f29766c8aca15bf9aa53b34db
Dave Brophy <dave@brophy.uk>
2018-05-04 14:19:52 +0200

    Add GA

--- server/handle-page.go
+++ server/handle-page.go
@@ -310,6 +310,13 @@ func (h *Handler) handlePlayPage(w http.ResponseWriter, req *http.Request) {
 var playPageTemplate = template.Must(template.New("main").Parse(`<html>
 	<head>
 		<meta charset="utf-8">
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118676357-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-118676357-1');
+        </script>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
